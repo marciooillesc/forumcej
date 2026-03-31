5f7c7641bd09637bd8efb464d851737260d0d486
marciooillesc <marciooillesc@gmail.com>
2026-03-31 00:28:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -80,9 +80,11 @@ function _ativarModo(modo) {
     // Navega para rota padrão do modo
     if (modo === 'jogos') {
       navegar('jogos-home');
-    } else {
-      navegar('professores');
-    }
+    } else if (modo === 'busca') {
+    navegar('busca');
+  } else {
+    navegar('professores');
+  }
   }, 350);
 }
 
