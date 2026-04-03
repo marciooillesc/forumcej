f41a9f90c7a52fb75a55b7a4faeb30ba5f5ae2a1
iprpr <marciooillesc@gmail.com>
2026-04-03 00:46:14 -0300

    Update supabase.js

--- modules/supabase.js
+++ modules/supabase.js
@@ -1,6 +1,5 @@
-
 const SUPABASE_URL = "https://gdjpwwmdfssvokuuibwz.supabase.co";
-const SUPABASE_KEY = "sb_publishable_pq2tmDPfr63IG-ymmpANRA_CRJRnrOU";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkanB3d21kZnNzdm9rdXVpYnd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNjExMjUsImV4cCI6MjA5MDYzNzEyNX0.JAHc5TVF4lR34nQ2UluQvYFZP8gxOvVDnM8_7mhTxl4";
 
 export const supabase = window.supabase.createClient(
   SUPABASE_URL,
